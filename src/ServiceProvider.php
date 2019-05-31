beecd35cc747a75f2545e307d6d1b8a06fb16cbb
sanjingshou <sxpd7788@gmail.com>
2019-05-31 14:45:48 +0800

    init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace Observer\LaravelPdd;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Boot the provider.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the provider.
+     */
+    public function register()
+    {
+        
+    }
+}
